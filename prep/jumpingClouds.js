91b3589196e9b3f736815c9a4f2d1b8e12932f76
Austn Hughes <austen.hughes.91@gmail.com>
2021-08-01 17:03:03 -0600

    jumping Clouds challange

---
+++ prep/jumpingClouds.js
@@ -0,0 +1,17 @@
+// 0 means jump
+// 1 means dont jump
+
+let path = [0,1,0,0,0,1,0];
+let hops = 0;
+
+for (let i = 1; i < path.length; i++) {
+    const element = path[i];
+    // console.log(element)
+    if(element===0){
+        hops = hops +1;
+    }else if(element===1){
+        hops = hops;
+    }
+}
+
+console.log(hops)
